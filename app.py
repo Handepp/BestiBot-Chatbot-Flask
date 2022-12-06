9f483d84b9c46a2abf32cd481379e9081d1e053d
Reyhan Devara <90141836+Handepp@users.noreply.github.com>
2022-12-07 00:55:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -37,5 +37,16 @@ def apiDeteksi():
     return res
 
 
-if __name__ == '__main__':    
+if __name__ == '__main__':   
+    #Pretrained Model
+    PRE_TRAINED_MODEL = 'indobenchmark/indobert-base-p2'
+
+    #Load tokenizer dari pretrained model
+    bert_tokenizer = BertTokenizer.from_pretrained(PRE_TRAINED_MODEL)
+
+    # Load hasil fine-tuning
+    bert_load_model = TFBertForSequenceClassification.from_pretrained(PRE_TRAINED_MODEL, num_labels=62)
+
+    #Load Model
+    bert_load_model.load_weights('bert-model.h5')
     app.run(debug=True, port=os.getenv("PORT", default=5000))
